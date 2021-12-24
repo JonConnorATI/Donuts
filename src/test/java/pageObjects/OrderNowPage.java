03f877fe13b7112fadf192660941249e11d68c3d
Jon Connor <jonconnor@live.ie>
2021-12-24 13:24:42 +0000

    Tidying up

--- src/test/java/pageObjects/OrderNowPage.java
+++ src/test/java/pageObjects/OrderNowPage.java
@@ -1,8 +1,6 @@
 package pageObjects;
 
 import java.time.Duration;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
