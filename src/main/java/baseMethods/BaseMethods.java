3b47219e6b50bab9420ce89b3087bc18d5fb8e15
Jon Connor <jonconnor@live.ie>
2022-03-22 10:43:43 +0000

    tidy up part2

--- src/main/java/baseMethods/BaseMethods.java
+++ src/main/java/baseMethods/BaseMethods.java
@@ -1,18 +1,10 @@
 package baseMethods;
 
 import java.time.Duration;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 //import cucumber.api.java.After;
 //import cucumber.api.java.Before;
